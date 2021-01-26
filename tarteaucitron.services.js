34368c6cd124f3a09378cd892324b97b035200ee
Tanguyel <tanguy.elmouahidine@gmail.com>
2021-01-26 10:09:23 +0100

    Ajout service sendinblue

--- tarteaucitron.services.js
+++ tarteaucitron.services.js
@@ -3340,3 +3340,41 @@ tarteaucitron.services.woopra = {
     }
 };
 
+//Sendinblue
+tarteaucitron.services.sendinblue = {
+  "key": "sendinblue",
+  "type": "analytic",
+  "name": "SendinBlue tracker",
+  "needConsent": true,
+  "cookies": ['sib_cuid'],
+  "js": function () {
+    "use strict";
+    // When user allow cookie
+      window.sib = {
+        equeue: [],
+        client_key: tarteaucitron.user.sendinblueID
+    };
+    /* OPTIONAL: email for identify request*/
+    if (tarteaucitron.user.sendinblueEmailID !== undefined) {
+            window.sib.email_id = tarteaucitron.user.sendinblueEmailID;
+        }
+    
+    window.sendinblue = {};
+    for (var j = ['track', 'identify', 'trackLink', 'page'], i = 0; i < j.length; i++) {
+    (function(k) {
+        window.sendinblue[k] = function() {
+            var arg = Array.prototype.slice.call(arguments);
+            (window.sib[k] || function() {
+                    var t = {};
+                    t[k] = arg;
+                    window.sib.equeue.push(t);
+                })(arg[0], arg[1], arg[2]);
+            };
+        })(j[i]);
+    }
+    var n = document.createElement("script"),
+        i = document.getElementsByTagName("script")[0];
+    n.type = "text/javascript", n.id = "sendinblue-js", n.async = !0, n.src = "https://sibautomation.com/sa.js?key=" + window.sib.client_key, i.parentNode.insertBefore(n, i), window.sendinblue.page();
+})();
+  },
+};
